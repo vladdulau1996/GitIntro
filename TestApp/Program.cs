f6962c728e26a4491e04a518a3655f033498dad9
Vlad Dulau <vlad.dulau@amdaris.com>
2022-11-03 14:28:41 +0200

    Add sum calculator

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,4 +3,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Tim's new line");
 Console.WriteLine("Rebeca's new line");
 
+int SumCalculator(int a, int b) => a + b;
+
 float floatCalculator(float a, float b) => a + b;
